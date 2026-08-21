8e9aa8014df86d1fff10b9ccfadabce3c0334a29
Apicehotel <appmanutenzioneapice@gmail.com>
2026-08-21 20:15:08 +0200

    Carica scala header responsive

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
 import './mockup-ui.css'
+import './header-scale.css'
 import { registerPwa } from './pwa.js'
 
 createRoot(document.getElementById('root')).render(
