550faa71bc1554e6783ed353b1200e55a06d753e
Apicehotel <appmanutenzioneapice@gmail.com>
2026-08-29 15:03:41 +0200

    route /randai to protected console

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { RandAIEntry } from './randai-entry.jsx'
 import './styles.css'
 import './mockup-ui.css'
 import './header-scale.css'
 import './brand-theme.css'
+import './randai-console.css'
 import { registerPwa } from './pwa.js'
+
+const path = window.location.pathname.replace(/\/+$/, '') || '/'
+const RootApp = path === '/randai' ? RandAIEntry : App
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RootApp />
   </React.StrictMode>,
 )
 
