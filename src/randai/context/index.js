3d2aaf359267d77a6def5f573bdfbe0f3db36b4f
Apicehotel <appmanutenzioneapice@gmail.com>
2026-08-30 23:15:25 +0200

    feat(randai): export operational context helpers

--- src/randai/context/index.js
+++ src/randai/context/index.js
@@ -1,1 +1,11 @@
 export { ContextEngine } from './engine.js'
+export {
+  buildOperationalContext,
+  publishOperationalContext,
+  clearOperationalContext,
+  getOperationalContext,
+  subscribeOperationalContext,
+  contextQueryHint,
+  OPERATIONAL_CONTEXT_EVENT,
+  OPERATIONAL_CONTEXT_VERSION,
+} from './operational-context.js'
