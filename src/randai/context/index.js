4a62ca35b85614c428973f60117c5e655005abd8
Apicehotel <appmanutenzioneapice@gmail.com>
2026-08-30 23:06:52 +0200

    feat(randai): export operational context envelope

--- src/randai/context/index.js
+++ src/randai/context/index.js
@@ -1,1 +1,11 @@
 export { ContextEngine } from './engine.js'
+export {
+  RANDAI_CONTEXT_EVENT,
+  RANDAI_CONTEXT_VERSION,
+  clearRandAIContextResource,
+  createIssueContextEnvelope,
+  createRandAIContextEnvelope,
+  getRandAIContext,
+  publishRandAIContext,
+  subscribeRandAIContext,
+} from './envelope.js'
