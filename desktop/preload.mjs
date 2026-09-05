5cd3931da9e0c592f7001130a1bd17d4eab8cc3d
Apicehotel <appmanutenzioneapice@gmail.com>
2026-09-05 10:30:00 +0200

    feat(randdesktop): expose narrow printing bridge

---
+++ desktop/preload.mjs
@@ -0,0 +1,16 @@
+import { contextBridge, ipcRenderer } from 'electron'
+
+const CHANNELS = Object.freeze({
+  printers: 'randdesktop:print:printers',
+  current: 'randdesktop:print:current',
+  document: 'randdesktop:print:document',
+})
+
+contextBridge.exposeInMainWorld('randDesktop', Object.freeze({
+  platform: process.platform,
+  print: Object.freeze({
+    listPrinters: () => ipcRenderer.invoke(CHANNELS.printers),
+    current: () => ipcRenderer.invoke(CHANNELS.current),
+    document: (document) => ipcRenderer.invoke(CHANNELS.document, document),
+  }),
+}))
