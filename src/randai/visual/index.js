0e2e784b6a97bc48c8e44be435f1a1b5127934b0
Apicehotel <appmanutenzioneapice@gmail.com>
2026-09-09 21:32:08 +0200

    feat(randvisual): expose governed source registry

--- src/randai/visual/index.js
+++ src/randai/visual/index.js
@@ -3,6 +3,13 @@ export { layoutRandVisual } from './layout.js'
 export { renderRandVisualSvg, assertSafeRandVisualSvg } from './renderer.js'
 export { RandVisualEngine, RandVisualScopeError, createRandVisualTool } from './engine.js'
 export {
+  RandVisualSourceRole,
+  RAND_VISUAL_SOURCES,
+  assertRandVisualSources,
+  getRandVisualSource,
+  createRandVisualImagePlan,
+} from './sources.js'
+export {
   RandCoreVisualView,
   RandCoreVisualEvidenceError,
   RandCoreVisualIntelligence,
